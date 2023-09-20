9053aef3f5a3d60cb7b124558b9e7d1d3b556b66
FranchuDev <franchiko1@gmail.com>
2023-09-20 00:28:38 -0300

    load styles navbar, home section 21:21min

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,14 @@ if (navClose) {
 		navMenu.classList.remove("show-menu");
 	});
 }
+
+// ==================================== REMOVE MENU MOBILE //
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+	//Cuando se hace click en cada link del menu, se borra la clase show-menu
+	const navMenu = document.getElementById("nav-menu");
+	navMenu.classList.remove("show-menu");
+};
+navLink.forEach((element) => {
+	element.addEventListener("click", linkAction);
+});
